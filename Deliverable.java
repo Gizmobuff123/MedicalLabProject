b9eeaa8d2fb9e3c1d8fc5a399d5728e0ab7acab6
Hassan10 <hmm851@gmail.com>
2016-01-12 20:49:50 +0500

    Create Deliverable.java

---
+++ Deliverable.java
@@ -0,0 +1,33 @@
+
+public class Deliverable extends Profile
+{
+	
+	private int testCode;
+	private String testReport;
+	public int getTestCode()
+	{
+		return testCode;
+	}
+	public void setTestCode(int testCode) 
+	{
+		this.testCode = testCode;
+	}
+	public String getTestReport() 
+	{
+		return testReport;
+	}
+	public void setTestReport(String testReport) 
+	{
+		this.testReport = testReport;
+	}
+	public Deliverable(int patientId, String name, String address, 
+			String contactNo, String email, String cnic, int testCode, String testReport) 
+	{
+		super(patientId, name, address, contactNo, email, cnic);
+		this.testCode=testCode;
+		this.testReport=testReport;	
+	}
+	
+	
+	
+}
