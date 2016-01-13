58fd638d46eb351cd2ac15b2c1b02646c0162dfb
Hassan10 <hmm851@gmail.com>
2016-01-13 12:21:50 +0500

    Create Finanace.java

---
+++ Finanace.java
@@ -0,0 +1,16 @@
+
+public class Finance 
+{
+	private static int amountInDashboard = 0;
+	
+	Finance(int t)
+	{
+		super();
+		amountInDashboard=amountInDashboard+t;
+	}
+
+	public static int getAmountInDashboard() 
+	{
+		return amountInDashboard;
+	}
+}
