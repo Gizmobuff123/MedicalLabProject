d270d3cdb388b5e8dcf7706e6b6508beab003a64
Hassan10 <hmm851@gmail.com>
2016-01-15 01:41:58 +0500

    Create TestType.java

---
+++ TestType.java
@@ -0,0 +1,37 @@
+package medicalLabEntities;
+
+public class TestType 
+{
+	private int testCode;
+	private int testRate;
+	private String description;
+	public int getTestCode() 
+	{
+		return testCode;
+	}
+	public void setTestCode(int testCode) 
+	{
+		this.testCode = testCode;
+	}
+	
+	
+	public int getTestRate() 
+	{
+		return testRate;
+	}
+	public void setTestRate(int testRate) 
+	{
+		this.testRate = testRate;
+	}
+	public String getDescription() 
+	{
+		return description;
+	}
+	public void setDescription(String description) 
+	{
+		this.description = description;
+	}
+	
+}
+
+
